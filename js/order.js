1272f464f72531bcc5f49df3c59bb690c4aebe57
Kyle Peterson <kylep9@uw.edu>
2013-11-24 23:50:00 -0800

    no message

--- js/order.js
+++ js/order.js
@@ -1,7 +1,20 @@
 
 // Document Ready
 $(function() {
-	$('.add-to-cart').click(addToCart());
+	$('.add-to-cart').click(function() {
+		var newCartItem = {
+			type: this.getAttribute('data-type'),
+			name: this.getAttribute('data-name'),
+			size: this.getAttribute('data-size'),
+			price: this.getAttribute('data-price')
+		};
+
+		// push the new item onto the carts items array
+		cart.items.push(newCartItem);
+
+		// Render item in the cart on the order page.
+		renderCart(cart, $('#cart-container'));
+	});
 
 	// Initialize Cart
 	var cart = {
@@ -13,20 +26,6 @@ $(function() {
 	}
 });
 
-function addToCart() {
-	var newCartItem = {
-		type: this.getAttribute('data-type'),
-		name: this.getAttribute('data-name'),
-		size: this.getAttribute('data-size'),
-		price: this.getAttribute('data-price')
-	};
-
-	// push the new item onto the carts items array
-	cart.items.push(newCartItem);
-
-	// Render item in the cart on the order page.
-	renderCart(cart, $('#cart-container'));
-}
 
 function renderCart(cart, container) {
 	var i, item, instance;
